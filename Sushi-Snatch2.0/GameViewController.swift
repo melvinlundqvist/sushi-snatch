f86818e670655e3be83ad35205a87e9898504261
Melvin Lundqvist <melvinlundqvist@n130-p20.eduroam.kth.se>
2018-05-09 13:54:20 +0200

    Smooth övergång från start screen

--- Sushi-Snatch2.0/GameViewController.swift
+++ Sushi-Snatch2.0/GameViewController.swift
@@ -21,10 +21,10 @@ class GameViewController: UIViewController {
     @IBOutlet weak var sushiTextlbl: UILabel!
     @IBOutlet weak var bakgrundStartScreen: UIImageView!
     @IBOutlet weak var eater: UIImageView!
-    @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var sushiBit1: UIImageView!
     @IBOutlet weak var pow: UIImageView!
     @IBOutlet weak var playLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
     
     
     
@@ -33,21 +33,39 @@ class GameViewController: UIViewController {
      takes action when "Play- button is pressed. Hides all the startscreen animations.
      **/
     @IBAction func playButtonIsPressed(_ sender: Any) {
-        bakgrundStartScreen.isHidden = true
-        snatch.isHidden = true
-        playButton.isHidden = true
-        sushiTextlbl.isHidden = true
-        eater.isHidden = false
-        sushiBit1.isHidden = false
-        pow.isHidden = true
-        playLabel.isHidden = true
         
+        //stuff that fades out
+        UIView.animate(withDuration: 0.5, animations: {
+            self.bakgrundStartScreen.alpha = 0
+            self.snatch.alpha = 0
+            self.playButton.alpha = 0
+            self.sushiTextlbl.alpha = 0
+            self.pow.alpha = 0
+            self.playLabel.alpha = 0
+        }) { (finished) in
+            self.bakgrundStartScreen.isHidden = finished
+            self.snatch.isHidden = finished
+            self.playButton.isHidden = finished
+            self.sushiTextlbl.isHidden = finished
+            self.pow.isHidden = finished
+            self.playLabel.isHidden = finished
+        }
         
-        UIView.animate(withDuration: 6, animations: {
+        // stuff that fades in
+        self.eater.alpha = 0
+        self.sushiBit1.alpha = 0
+        self.eater.isHidden = false
+        self.sushiBit1.isHidden = false
+        UIView.animate(withDuration: 0.5, animations: {
+            self.eater.alpha = 1
+            self.sushiBit1.alpha = 1
+        })
+        
+        // sushi starts falling
+        UIView.animate(withDuration: 5, animations: {
             self.sushiBit1.frame.origin.y += 900
         }, completion: nil)
     }
-    
 
     var location = CGPoint(x: 0, y: 0)
     var eaterHeight = 466
@@ -58,19 +76,19 @@ class GameViewController: UIViewController {
         eater.center = CGPoint(x: location.x, y: CGFloat(eaterHeight))
     }
     
+    func collision() {
+        while 0==0{
+            if sushiBit1.frame.intersects(eater.frame) {
+                self.sushiBit1.isHidden = true
+            }
+        }
+    }
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch : UITouch! = touches.first
         location = touch.location(in: self.view)
         eater.center = CGPoint(x: location.x, y: CGFloat(eaterHeight))
     }
-    
-    
-    
-    
-    
-    
-    
-    
     
     override func viewDidLoad() {
            eater.center = CGPoint(x: self.view.bounds.midX, y: CGFloat(eaterHeight)) //ätarens startposition
